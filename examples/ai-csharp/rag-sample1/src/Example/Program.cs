24613b6c623296cc439d27fbcb79cee6ed721e10
Amin Mesbahi <amin.mesbahi@gmail.com>
2025-03-24 00:07:07 +0100

    wip

--- examples/ai-csharp/rag-sample1/src/Example/Program.cs
+++ examples/ai-csharp/rag-sample1/src/Example/Program.cs
@@ -4,6 +4,7 @@
 #pragma warning disable SKEXP0011
 #pragma warning disable SKEXP0050
 #pragma warning disable SKEXP0052
+#pragma warning disable 
 
 using HtmlAgilityPack;
 using Microsoft.Extensions.Configuration;
@@ -11,69 +12,72 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.KernelMemory;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.Ollama;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.Embeddings;
 using Microsoft.SemanticKernel.Memory;
 using Microsoft.SemanticKernel.Plugins.Memory;
+using System.Globalization;
 
-var question = "with what price can I buy 1 Euro using Rial?";
-Console.WriteLine($"I will try to answer the question: {question}");
-Console.WriteLine("First, with asking the question directly to the Phi-3 model via Ollama");
-Console.WriteLine("Second, get actual data from bonbast, and then use semantic memory and ask the question again");
-Console.WriteLine("");
+var question = "Do you know what the exchange rate is for 1 Euro compared to 1 Iranian rial?";
+Console.WriteLine($"❓ our question is: {question}");
+Console.WriteLine("1st: ask directly from llama3.2 (3 billion)");
+Console.WriteLine("2nd: ask via RAG technique with llama3.2 (3 billion)");
+Console.WriteLine("***************************************");
 
-// Create a chat completion service
 var builder = Kernel.CreateBuilder();
-builder.AddOpenAIChatCompletion(
-    modelId: "phi3.5",
-    endpoint: new Uri("http://localhost:11434"),
-    apiKey: "apikey");
-builder.AddLocalTextEmbeddingGeneration();
+builder.AddOllamaChatCompletion(
+    modelId: "llama3.2:3b",
+    endpoint: new Uri("http://127.0.0.1:11434"));
+
+builder.AddOllamaTextEmbeddingGeneration(modelId: "llama3.2:3b",
+    endpoint: new Uri("http://127.0.0.1:11434")
+    );
+
 Kernel kernel = builder.Build();
 
-Console.WriteLine($"Phi-3 response (no memory).");
+Console.WriteLine($"llama3.2's answer:");
 var response = kernel.InvokePromptStreamingAsync(question);
 await foreach (var result in response)
 {
     Console.Write(result);
 }
 
-// separator
 Console.WriteLine("");
-Console.WriteLine("==============");
-Console.WriteLine("Press Enter to continue");
+Console.WriteLine("***************************************");
+Console.WriteLine("Press Enter to use RAG");
 Console.ReadLine();
-Console.WriteLine("==============");
-Console.WriteLine($"Phi-3 response (using semantic memory).");
+Console.WriteLine("***************************************");
+Console.WriteLine($"llama3.2's answer with RAG (using semantic memory):");
 Console.WriteLine("");
 
 var embeddingGenerator = kernel.Services.GetRequiredService<ITextEmbeddingGenerationService>();
 var memory = new SemanticTextMemory(new VolatileMemoryStore(), embeddingGenerator);
 
+const string MemoryCollectionName = "exchangeRates";
 
 string url = "https://bonbast.org";
 var exchangeRates = await GetExchangeRatesAsync(url);
 
-const string MemoryCollectionName = "exchangeRates";
-int counter =0;
+int counter = 0;
 foreach (var rate in exchangeRates)
 {
-    await memory.SaveInformationAsync(MemoryCollectionName, id: $"rate{counter}", text: $"Currency: {rate.Key}, Average Rate: {rate.Value.AverageRate}, Last Update: {rate.Value.LastUpdate}");
+    string memoryKey = $"{rate.Key} to Iranian Rial";
+    await memory.SaveInformationAsync(MemoryCollectionName, id: memoryKey, text: $"1 {rate.Key} equals {rate.Value.AverageRate * 10} Iranian Rials...");
+    counter += 1;
 }
-
+Console.WriteLine($"{counter} rates added to the memory... wait please");
 TextMemoryPlugin memoryPlugin = new(memory);
 
 kernel.ImportPluginFromObject(memoryPlugin);
 
-OpenAIPromptExecutionSettings settings = new()
-{
-    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
-};
-
+OllamaPromptExecutionSettings settings = new OllamaPromptExecutionSettings();
 
 var prompt = @"
     Question: {{$input}}
-    Answer the question using the memory content: {{Recall}}";
+    Retrieved Data: {{Recall}}
+
+    Using the above retrieved data, please provide the current exchange rate for 1 Euro compared to 1 Iranian Rial.";
 
 var arguments = new KernelArguments(settings)
 {
@@ -81,16 +85,22 @@ var arguments = new KernelArguments(settings)
     { "collection", MemoryCollectionName }
 };
 
+string[] currencyPairs = { "Euro to Iranian Rial", "EUR to IRR", "Euro/IRR" };
 
+foreach (var pair in currencyPairs)
+{
+    if (question.Contains(pair))
+    {
+        arguments.Add("currencyPair", pair);
+        break;
+    }
+}
 
 response = kernel.InvokePromptStreamingAsync(prompt, arguments);
 await foreach (var result in response)
 {
     Console.Write(result);
 }
-
-Console.WriteLine($"");
-
 
 
 static async Task<Dictionary<string, (double AverageRate, string LastUpdate)>> GetExchangeRatesAsync(string url)
